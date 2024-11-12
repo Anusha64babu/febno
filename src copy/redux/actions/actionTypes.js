7a08c7d510331fc96eaa183138205c2d547e407b
Anusha64babu <119351822+Anusha64babu@users.noreply.github.com>
2024-11-12 11:14:51 +0530

    Delete src copy directory

--- src copy/redux/actions/actionTypes.js
+++
@@ -1,6 +0,0 @@
-export const FETCH_BOOKS = 'FETCH_BOOKS';
-export const ADD_BOOK = 'ADD_BOOK';
-export const UPDATE_BOOK = 'UPDATE_BOOK';
-export const DELETE_BOOK = 'DELETE_BOOK';
-export const SET_LOADING = 'SET_LOADING';
-export const SET_ERROR = 'SET_ERROR';
